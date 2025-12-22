b7b5b1137a4234cd8d287bb5e456bdf5cb7cc79e
FixatedEverVT <fixated_ever@thepettablesvt.com>
2025-12-22 06:52:03 -0800

    Update discord.js

--- src/generators/discord.js
+++ src/generators/discord.js
@@ -11,22 +11,9 @@ import {Order} from 'blockly/javascript';
 // This file has no side effects!
 export const forBlock = Object.create(null);
 
-forBlock['add_text'] = function (block, generator) {
+forBlock['comment'] = function (block, generator) {
   const text = generator.valueToCode(block, 'TEXT', Order.NONE) || "''";
-  const addText = generator.provideFunction_(
-    'addText',
-    `function ${generator.FUNCTION_NAME_PLACEHOLDER_}(text) {
-
-  // Add text to the output area.
-  const outputDiv = document.getElementById('output');
-  const textEl = document.createElement('p');
-  textEl.innerText = text;
-  outputDiv.appendChild(textEl);
-}`,
-  );
-  // Generate the function call for this block.
-  const code = `${addText}(${text});\n`;
-  return code;
+  return `//${text}\n`;
 };
 forBlock['discord_send_message'] = function (block, generator) {
   const message = generator.valueToCode(block, 'MESSAGE', Order.NONE) || "''";
