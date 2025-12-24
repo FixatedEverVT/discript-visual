88933ba635006575ea27fe26bb1c14adf8d5d861
FixatedEverVT <fixated_ever@thepettablesvt.com>
2025-12-24 03:10:23 -0800

    Update build.js

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 import { spawn, execSync } from "child_process";
-const VERSION = "-1.1.0";
+const VERSION = "-1.3.0";
 const A64 = "_arm64";
 const AHF = "_armhf";
 const X64 = "_x64";
@@ -13,7 +13,7 @@ function makePaths (platform, type, extension) {
 	return ["discript-" + platform + type + extension, "discript-" + platform + type + VERSION + ".zip"]
 }
 function makeCommand (target, location) {
-	return `Compress-Archive -Path 'dist/discript/${target}','dist/discript/resources.neu' -DestinationPath 'dist/discript/${location}' -Force`;
+	return `Compress-Archive -Path 'dist/discript/${target}','dist/discript/resources.neu' -DestinationPath 'dist/zips/${location}' -Force`;
 }
 function zippers (code) {
 	if (code !== 0) { console.error("neu build failed"); return; }
